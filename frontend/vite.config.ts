91c54fb1348c0cdd717f846ae9205c61e1c13295
hidakayu <hidakayu@systena.co.jp>
2025-08-18 10:18:35 +0900

    Re-add files based on updated .gitignore

---
+++ frontend/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        preserveSymlinks: true
+    },
+    build: {
+        outDir: "../backend/static",
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: id => {
+                    if (id.includes("@fluentui/react-icons")) {
+                        return "fluentui-icons";
+                    } else if (id.includes("@fluentui/react")) {
+                        return "fluentui-react";
+                    } else if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                }
+            }
+        },
+        target: "esnext"
+    },
+    // サーバー設定（プロキシを含む）
+    server: {
+        proxy: {
+            // "/api" で始まるリクエストは、すべてバックエンドに転送する
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+            },
+        },
+    },
+});
