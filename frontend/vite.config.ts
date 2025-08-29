d64068e940a589a49cf16732201d51a4d2c63a5a
hidakayu <hidakayu@systena.co.jp>
2025-08-29 22:25:02 +0900

    再ビルド

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,19 +12,19 @@ export default defineConfig({
         outDir: 'build',
         emptyOutDir: true,
         sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks: id => {
-                    if (id.includes("@fluentui/react-icons")) {
-                        return "fluentui-icons";
-                    } else if (id.includes("@fluentui/react")) {
-                        return "fluentui-react";
-                    } else if (id.includes("node_modules")) {
-                        return "vendor";
-                    }
-                }
-            }
-        },
+        // rollupOptions: {
+        //     output: {
+        //         manualChunks: id => {
+        //             if (id.includes("@fluentui/react-icons")) {
+        //                 return "fluentui-icons";
+        //             } else if (id.includes("@fluentui/react")) {
+        //                 return "fluentui-react";
+        //             } else if (id.includes("node_modules")) {
+        //                 return "vendor";
+        //             }
+        //         }
+        //     }
+        // },
         target: "esnext"
     },
     // サーバー設定（プロキシを含む）
